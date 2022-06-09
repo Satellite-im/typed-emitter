000f4a6a574ccabd97f64fab1d148b6856853dde
mtumiati <manuel.tumiati@knobs.it>
2022-06-09 11:00:22 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# typed-emitter
+
+Strongly typed event emitter
+
+## ⚙️ Install
+
+Install it locally in your project folder:
+
+```bash
+npm i typed-emitter
+# Or Yarn
+yarn add typed-emitter
+```
+
+## 📖 Usage
+
+### Declare event types
+
+```typescript
+interface MyListeners {
+  'user:connect': (data: { userId: string }) => void
+  'user:message': (data: { userId: string, message: string }) => void
+}
+```
+
+### Inherit from emitter using generics
+
+```typescript
+class MyClass extends Emitter<MyListeners> {
+    constructor() {
+        // my initializations
+    }
+
+    connect() {
+        // ...
+        this.emit('user:connect', {userId: "591"}); // <- payload is strongly typed
+    }
+
+    onMessageReceived(userId: string, message: string) {
+        // ...
+        this.emit('user:message', {userId, message}) // <- payload is strongly typed
+    }
+}
+```
+
+### Subscribe to events in a strongly typed way
+
+```typescript
+const myClass = new MyClass();
+
+myClass.on('user:connect', ({userId})=>{ // <- the callback signature is strongly typed
+    // your code here
+})
+
+myClass.on('user:message', ({userId, message})=>{ // <- the callback signature is strongly typed
+    // your code here
+})
+```
+
+
+## 💬 Discussions
+
+Join our discord server [here](https://discord.gg/NwrjYHj8) to share your ideas.
+
+## License
+
+MIT &copy; [Satellite.im](https://satellite.im)
---
+++ src/emitter.ts
@@ -0,0 +1,64 @@
+/**
+ * @class Emitter
+ * @description The emitter class provides an interface for
+ * listening and emitting custom events in a strongly typed way
+ */
+ export default class Emitter<
+ Listeners extends { [key in keyof Listeners]: (...args: any[]) => any },
+> {
+ private _events: { [key in keyof Listeners]?: Array<Listeners[key]> } = {}
+
+ /**
+  * @method on
+  * @description Bind event listeners
+  * @param event Name of the event to listen to
+  * @param listener to call on any Event
+  * @example Emitter.on('EVENT_NAME', (parameter: ParameterType) => {})
+  */
+ on<T extends keyof Listeners>(event: T, listener: Listeners[T]) {
+   if (!this._events[event]) {
+     this._events[event] = []
+   }
+
+   this._events[event]!.push(listener)
+ }
+
+ /**
+  * @method off
+  * @description Removes an event listener from the listener box
+  * @param event Name of the event to unsubscribe from
+  * @param listener Listener function to remove
+  * @example Emitter.off("EVENT_NAME", (parameter: ParameterType) => {})
+  */
+ off<T extends keyof Listeners>(event: T, listenerToRemove: Listeners[T]) {
+   if (!this._events[event]) {
+     throw new Error(
+       `Can't remove a listener. Event "${event.toString()}" doesn't exits.`,
+     )
+   }
+
+   const filteredListeners = this._events[event]!.filter(
+     (listener: Listeners[T]) => listener !== listenerToRemove,
+   )
+
+   this._events[event] = filteredListeners
+ }
+
+ /**
+  * @method emit
+  * @description Emits an event to all listeners
+  * @param event Name of the event to emit
+  * @param data Data to provide to listeners
+  * @example Emitter.emit('EVENT_NAME', {eventParam, otherParam})
+  */
+ protected emit<T extends keyof Listeners>(
+   event: T,
+   ...[data]: Parameters<Listeners[T]>
+ ) {
+   if (!this._events[event]) {
+     return
+   }
+
+   this._events[event]!.forEach((cb) => cb(data))
+ }
+}
